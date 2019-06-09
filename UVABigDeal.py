392ee13922970abc03a321f713db011aa6804c7a
Erich Purpur <epurpur@gmail.com>
2019-06-09 14:17:35 -0400

    Add files via upload

--- UVABigDeal.py
+++ UVABigDeal.py
@@ -14,6 +14,7 @@ import pandas as pd
 import matplotlib as mpl
 import matplotlib.pyplot as plt
 import numpy as np
+from operator import itemgetter
 
 def read_jr5():
     """Make 'Downloads JR5 2017 in 2017' table. JR5 data is all downloads by provider of 2017 articles in 2017"""
@@ -183,6 +184,62 @@ def read_jr1_no_elsevier():
     print(f"{len(under_100k_downloads)} of {len(jr1_totals)} providers have less than 100,000 downloads. ({round(percent_under_100k_downloads, 2)}%)")
 
 
+def jr1_big5():
+    """Showing jr1 data for big 5 providers only
+    Big 5 are: Elsevier, Wiley, Springer, Taylor & Francis, Sage"""
+    
+    
+    data = pd.read_csv('Packages.csv', skiprows=8)
+    
+    jr1_data_by_package = data.groupby(['Provider', 'Downloads JR1 2017'], as_index=False).sum().values.tolist()
+    jr1_data = []
+
+    big5 = ['Elsevier', 'Wiley', 'Springer', 'Taylor & Francis', 'Sage']
+    for i in jr1_data_by_package:
+        if i[0] in big5:
+            jr1_data.append(i)
+    
+    big5_packages = [x[0] for x in jr1_data]
+    big5_totals = [x[1] for x in jr1_data]
+    
+    mpl.rcParams['ytick.major.width'] = 1
+    mpl.rcParams['xtick.major.width'] = 1
+    plt.figure(num=None, figsize=(8,8))
+    plt.suptitle('Downloads JR1 in 2017 (Big5 Providers)')
+    plt.barh(big5_packages, big5_totals, height=.8, color='green')
+    plt.grid()
+    plt.show()
+
+
+def jr1_other_providers():
+    """Showing jr1 data for all other providers outside big 5"""
+    
+    
+    data = pd.read_csv('Packages.csv', skiprows=8)
+    
+    jr1_data_by_package = data.groupby(['Provider', 'Downloads JR1 2017'], as_index=False).sum().values.tolist()
+    jr1_data = []
+    
+    big5 = ['Elsevier', 'Wiley', 'Springer', 'Taylor & Francis', 'Sage']
+    for i in jr1_data_by_package:
+        if i[0] not in big5:
+            jr1_data.append(i)
+  
+    jr1_packages = [x[0] for x in jr1_data_by_package]
+    jr1_totals = [x[1] for x in jr1_data_by_package]
+    
+    jr1_packages = [x[0] for x in jr1_data]
+    jr1_totals = [x[1] for x in jr1_data]
+    
+    mpl.rcParams['ytick.major.width'] = 1
+    mpl.rcParams['xtick.major.width'] = 1
+    plt.figure(num=None, figsize=(8,8))
+    plt.suptitle('Downloads JR1 in 2017 (Not Big5 Providers)')
+    plt.barh(jr1_packages, jr1_totals, height=.8, color='green')
+    plt.grid()
+    plt.show()  
+
+
 def jr1_jr5_big5_grouped_bar_chart():
     """Grouped bar chart showing comparison of jr5 vs jr1 downloads by provider in 2017 for the big 5 providers.
     Big 5 are: Elsevier, Wiley, Springer, Taylor & Francis, Sage""" 
@@ -215,24 +272,101 @@ def jr1_jr5_big5_grouped_bar_chart():
     jr5_position = [x + bar_width for x in jr1_position]
     
     plt.figure(num=None, figsize=(8,8))
-    plt.bar(jr1_position, jr1_totals, color='blue', width=bar_width)
-    plt.bar(jr5_position, jr5_totals, color='red', width=bar_width)
+    plt.suptitle('2017 downloads by Provider: JR1 vs. JR5')
+    plt.bar(jr1_position, jr1_totals, color='blue', width=bar_width, label='JR1 downloads')
+    plt.bar(jr5_position, jr5_totals, color='red', width=bar_width, label='JR5 downloads')
     plt.xticks([r+bar_width for r in range(len(packages))], packages)
     plt.xticks(rotation=90)
     
 
+    plt.legend()            #grabs the 'label' argument from plt.bar
+    plt.show()
+    
+
 def jr1_jr5_other_providers_grouped_bar_chart():
-   pass
+   """Grouped bar chart showing comparison of jr5 and jr1 downloads by provider inn 2017 for all providers
+   outside big 5"""
+       
+   data = pd.read_csv('Packages.csv', skiprows=8)
+    
+   jr5_data_by_package = data.groupby(['Provider', 'Downloads JR5 2017 in 2017'], as_index=False).sum().values.tolist()
+   jr1_data_by_package = data.groupby(['Provider', 'Downloads JR1 2017'], as_index=False).sum().values.tolist()
 
-#read_jr5()
-#read_jr5_no_elsevier()
-#jr5_big5()
-#jr5_other_providers()
+   jr5_big5_data = []
+   jr1_big5_data = []
 
-#read_jr1()
-#read_jr1_no_elsevier()
+   big5 = ['Elsevier', 'Wiley', 'Springer', 'Taylor & Francis', 'Sage']
+   for i in jr5_data_by_package:
+       if i[0] not in big5:
+           jr5_big5_data.append(i)
 
-#jr1_jr5_big5_grouped_bar_chart() 
+   for i in jr1_data_by_package:
+       if i[0] not in big5:
+           jr1_big5_data.append(i)
+
+   packages = [x[0] for x in jr1_big5_data]
+   jr1_totals = [x[1] for x in jr1_big5_data]
+   jr5_totals = [x[1] for x in jr5_big5_data]
+
+   bar_width = .25
+    
+   #set position of bar on X axis
+   jr1_position = np.arange(len(jr1_totals))
+   jr5_position = [x + bar_width for x in jr1_position]
+    
+   plt.figure(num=None, figsize=(8,8))
+   plt.suptitle('2017 downloads by Provider: JR1 vs. JR5')
+   plt.bar(jr1_position, jr1_totals, color='blue', width=bar_width, label='JR1 downloads')
+   plt.bar(jr5_position, jr5_totals, color='red', width=bar_width, label='JR5 downloads')
+   plt.xticks([r+bar_width for r in range(len(packages))], packages)
+   plt.xticks(rotation=90)
+    
+
+   plt.legend()            #grabs the 'label' argument from plt.bar
+   plt.show()
+    
+
+def percent_jr5_of_jr1():
+    """Print list of providers, showing ratio of jr5 downloads as % of all downloads(jr1) in 2017"""
+    
+    data = pd.read_csv('Packages.csv', skiprows=8)
+    
+    jr5_data_by_package = data.groupby(['Provider', 'Downloads JR5 2017 in 2017'], as_index=False).sum().values.tolist()
+    jr1_data_by_package = data.groupby(['Provider', 'Downloads JR1 2017'], as_index=False).sum().values.tolist()
+    
+    combined = list(zip(jr5_data_by_package, jr1_data_by_package))
+    
+#    print(combined[0][0][1])
+#    print(combined[0][0][4])
+#    print((combined[0][0][1])/(combined[0][0][4]))
+    
+    final_rank = []
+    
+    for i in combined:
+        final_rank.append((i[0][0], round((i[0][1]/i[0][4]),4)))        #round to 4 decimal places
+        
+    final_rank_sorted = tuple(sorted(final_rank, key=itemgetter(1), reverse=True)   )
+    
+    print("% JR5 downloads of total downloads in 2017 (JR1) by Provider \n")
+    for i in final_rank_sorted:
+        print(i[0], "-" , i[1])
+
+
+read_jr5()
+read_jr5_no_elsevier()
+jr5_big5()
+jr5_other_providers()
+
+read_jr1()
+read_jr1_no_elsevier()
+jr1_big5()
+jr1_other_providers()
+
+
+jr1_jr5_big5_grouped_bar_chart() 
+jr1_jr5_other_providers_grouped_bar_chart()
+        
+percent_jr5_of_jr1()
 
 
 
--- UVABigDeal2.py
+++ UVABigDeal2.py
@@ -9,6 +9,8 @@ Created on Tue Jun  4 15:18:41 2019
 import pandas as pd
 import matplotlib.pyplot as plt
 import numpy as np
+from operator import itemgetter
+
 
 data = pd.read_csv("JournalsPerProvider.csv", skiprows=8)
 
@@ -16,37 +18,16 @@ data = pd.read_csv("JournalsPerProvider.csv", skiprows=8)
 #result1 = data[data['Provider'] == 'AIP'].groupby('Field', as_index=False).sum().values.tolist()
 #print(result1)
 
-data = pd.read_csv('Packages.csv', skiprows=8)
+my_tuple = (
+    ('A','Apple'),
+    ('C','Carrot'),
+    ('B','Banana'),
+)
+
+print(my_tuple)
+
+my_tuple_sorted = tuple(sorted(my_tuple, key=itemgetter(1)))
+
+print(my_tuple_sorted)
 
 
-import numpy as np
-import matplotlib.pyplot as plt
- 
-# set width of bar
-barWidth = 0.2
- 
-# set height of bar
-bars1 = [12, 30, 1, 8, 22]
-bars2 = [28, 6, 16, 5, 10]
-bars3 = [29, 3, 24, 25, 17]
- 
-# Set position of bar on X axis
-r1 = np.arange(len(bars1))
-r2 = [x + barWidth for x in r1]
-r3 = [x + barWidth for x in r2]
- 
-print(r2)
-print(r3)
-
-# Make the plot
-plt.bar(r1, bars1, color='#7f6d5f', width=barWidth, edgecolor='white', label='var1')
-plt.bar(r2, bars2, color='#557f2d', width=barWidth, edgecolor='white', label='var2')
-plt.bar(r3, bars3, color='#2d7f5e', width=barWidth, edgecolor='white', label='var3')
- 
-# Add xticks on the middle of the group bars
-plt.xlabel('group', fontweight='bold')
-plt.xticks([r + barWidth for r in range(len(bars1))], ['A', 'B', 'C', 'D', 'E'])
- 
-# Create legend & Show graphic
-#plt.legend()
-#plt.show()
